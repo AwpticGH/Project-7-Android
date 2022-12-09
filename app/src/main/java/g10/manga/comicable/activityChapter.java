e3c900c3577fbe18cb9581b72cb7513d4bc451c2
Wahyu <whyuuuuau@gmail.com>
2022-12-09 21:31:38 +0700

    progress

---
+++ app/src/main/java/g10/manga/comicable/activityChapter.java
@@ -0,0 +1,14 @@
+package g10.manga.comicable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityChapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter);
+    }
+}
